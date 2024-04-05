d7193646ed95137aade23c76d574f08e6e90dc28
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-04-05 18:02:25 +0545

    interface, type, function return, parameters, type assertions, unknown, any, tuple, union

--- Typescript/01.ts
+++ Typescript/01.ts
@@ -36,3 +36,91 @@ let c = "name" as string; // specify that c always holds string by ourself
 function isCanVote(age: number): boolean {
   return age >= 18;
 }
+
+// type
+
+// prefer PascalCase for defining type
+type Gender = "MALE" | "FEMALE";
+
+let citizenGender: Gender;
+
+citizenGender = "FEMALE";
+
+// Object type
+
+type Area = {
+  length: number;
+  breadth: number;
+  dimension: `${number}x${number}`;
+};
+
+function calculateArea(value: Area) {
+  return value.breadth * value.breadth;
+}
+
+calculateArea({ length: 23, breadth: 11, dimension: "33x22" });
+// calculateArea({ length: 23, breadth: 11, dimension: "33d22" });  Error cause dimension format mismatched
+
+// Object
+type Developer = {
+  name: string;
+  hobbides: string[];
+  isCertified: boolean;
+  description: () => number; // void is here return type of function
+};
+
+const Gaurab: Developer = {
+  name: "Gaurab Khanal",
+  hobbides: ["chess", "cricket"],
+  isCertified: true,
+  description: function () {
+    return 1;
+  },
+};
+
+// Custom Types
+type Animal = {
+  name: string;
+};
+
+type Bear = Animal & {
+  // & is also called intersection
+  food: "Fish" | "Grass";
+};
+
+let polarBear: Bear = {
+  name: "PolarBear 1",
+  food: "Fish",
+};
+
+// Interface
+// prefer PascalCase
+interface IAnimal {
+  name: string;
+}
+
+interface IBear extends IAnimal {
+  food: "Fish" | "Grass";
+}
+
+let PolarBear: IBear = {
+  name: "Bear1",
+  food: "Grass",
+};
+
+// same Interface can be defined multiple times
+// this will combine all the properties of existing interface to new one.
+// this isnt available for type
+
+interface IWindow {
+  title: string;
+}
+
+interface IWindow {
+  ts: string;
+}
+
+let win: IWindow = {
+  title: "Hello",
+  ts: "def",
+};
