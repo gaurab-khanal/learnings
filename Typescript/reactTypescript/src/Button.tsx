7810f7d3f41abf5692a5c88a1d076a49e96551ce
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-04-07 22:06:14 +0545

    pass props to custom element type components

--- Typescript/reactTypescript/src/Button.tsx
+++ Typescript/reactTypescript/src/Button.tsx
@@ -2,8 +2,12 @@ type ButtonProps = React.ComponentProps<"button"> & { variant: string };
 // if you wnat to add new attributes to component like button which dont exist by default
 // use intersection & and add type of attribute you want to add
 
-const Button = ({ type }: ButtonProps) => {
-  return <button type={type}>+</button>;
+const Button = ({ type, ...rest }: ButtonProps) => {
+  return (
+    <button type={type} {...rest}>
+      +
+    </button>
+  );
 };
 
 export default Button;
