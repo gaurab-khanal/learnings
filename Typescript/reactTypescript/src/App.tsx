53e3c0caf097cf89569f6cce201c7cb85c8f7067
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-04-07 22:05:39 +0545

    pass props to custom element type components

--- Typescript/reactTypescript/src/App.tsx
+++ Typescript/reactTypescript/src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 import Button from "./Button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       Hello world {count}
-      <Button setCount={setCount} />
+      <Button type="submit" />
     </>
   );
 }
--- Typescript/reactTypescript/src/Button.tsx
+++ Typescript/reactTypescript/src/Button.tsx
@@ -1,9 +1,9 @@
-const Button = ({
-  setCount,
-}: {
-  setCount: React.Dispatch<React.SetStateAction<number>>;
-}) => {
-  return <button onClick={() => setCount((prv) => prv + 1)}>+</button>;
+type ButtonProps = React.ComponentProps<"button"> & { variant: string };
+// if you wnat to add new attributes to component like button which dont exist by default
+// use intersection & and add type of attribute you want to add
+
+const Button = ({ type }: ButtonProps) => {
+  return <button type={type}>+</button>;
 };
 
 export default Button;
