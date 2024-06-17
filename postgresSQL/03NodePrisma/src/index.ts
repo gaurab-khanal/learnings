0c6aa24a1ce68bcb5bb0967a4d5726521f0a205e
gaurab-khanal <khanalgaurab98@gmail.com>
2024-06-17 20:48:04 +0545

    prisma relations

--- postgresSQL/03NodePrisma/src/index.ts
+++ postgresSQL/03NodePrisma/src/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const insertUser = async (username: string, password: string, firstName: string, lastName: string, email: string )=>{
+const insertUser = async (username: string, password: string, firstName: string, lastName: string, email: string) => {
     const user = await prisma.user.create({
         data: {
             username,
@@ -10,20 +10,28 @@ const insertUser = async (username: string, password: string, firstName: string,
             firstName,
             lastName,
             email
+        },
+        select: {
+            id: true,
+            username: true,
+            password: false,
+            firstName: true,
+            lastName: true,
+            email: true
         }
     })
     console.log(user)
 }
 
-insertUser("Saugat", "password", "Saugat", "Karki", "saugat@gmail.com");
+// insertUser("Saugat", "password", "Saugat", "Karki", "saugat@gmail.com");
 
-const getUserByEmail = async (email: string)=>{
+const getUserByEmail = async (email: string) => {
     const user = await prisma.user.findUnique({
         where: {
-           email: email
+            email: email
         }
     })
-    if(!user){
+    if (!user) {
         console.log("No user found")
         return
     }
@@ -32,14 +40,23 @@ const getUserByEmail = async (email: string)=>{
 
 // getUserByEmail("saugat@gmail.com")
 
-const getAllUsers = async ()=>{
-    const users = await prisma.user.findMany()
+const getAllUsers = async () => {
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            username: true,
+            firstName: true,
+            lastName: true,
+            email: true,
+            password: false
+        }
+    })
     console.log(users)
 }
 
 // getAllUsers()
 
-const updateOneUserByEmail = async (email: string, data: {username?: string, password?: string, firstName?: string, lastName?: string, email?: string})=>{
+const updateOneUserByEmail = async (email: string, data: { username?: string, password?: string, firstName?: string, lastName?: string, email?: string }) => {
     const user = await prisma.user.update({
         where: {
             email: email
@@ -51,7 +68,7 @@ const updateOneUserByEmail = async (email: string, data: {username?: string, pas
 
 // updateOneUserByEmail("saugat@gmail.com", {firstName: "saugat", username: "hojo"})
 
-const deleteUserByEmail = async (email: string)=>{
+const deleteUserByEmail = async (email: string) => {
     const user = await prisma.user.delete({
         where: {
             email: email
@@ -61,3 +78,47 @@ const deleteUserByEmail = async (email: string)=>{
 }
 
 // deleteUserByEmail("saugat@gmail.com")
+
+const insertTodo = async (title: string, completed: boolean, description: string, userId: number) => {
+    try {
+        const res = await prisma.todo.create({
+            data: {
+                title,
+                completed,
+                description,
+                userId
+            }
+        })
+        console.log(res)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// insertTodo("Learn Prisma", false, "Learn Prisma with Node.js", 1)
+
+const getTodos = async (userId: number) => {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId: userId
+        }
+    })
+    console.log(todos)
+}
+
+// getTodos(1)
+
+const getUserWithTodo = async (userId: number) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId
+        },
+        include: {
+            Todo: true
+        }
+    })
+    console.log(user)
+}
+
+getUserWithTodo(1)
